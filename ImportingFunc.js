5552b141205421423d9def9e9d5bf55fff502ee8
seyeoyadiran <seyeoyadiran@gmail.com>
2024-11-05 20:24:02 -0500

    First and Last Commit

---
+++ ImportingFunc.js
@@ -0,0 +1,10 @@
+import { returnSum, findAverage, stringsLongerThan, recurseBetween  } from "./ThinkFunc.js";
+
+
+const arrayThis = [12,23,12,45,7,1]
+returnSum(arrayThis)
+
+
+console.log(stringsLongerThan(["Word", "Perscholas", "Google", "I will get hired soon", "Lets get this money", "yurr"], 5))
+
+recurseBetween(1,30)
